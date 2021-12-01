3bd2681e047e721a062a1db5e9dd1c3ad0558b42
Jamalam360 <56727311+Jamalam360@users.noreply.github.com>
2021-12-01 08:15:03 +0000

    Finalise gradle.properties test

--- src/test/java/io/github/jamalam360/notify/test/GradlePropertiesTests.java
+++ src/test/java/io/github/jamalam360/notify/test/GradlePropertiesTests.java
@@ -15,6 +15,7 @@ public class GradlePropertiesTests {
     private final ModMetadata test3 = new GradlePropertiesModMetadata("https://raw.githubusercontent.com/JamCoreModding/NotifyTesting/main/gradle.properties/3.properties", "mod_version");
     private final ModMetadata test4 = new GradlePropertiesModMetadata("https://raw.githubusercontent.com/JamCoreModding/NotifyTesting/main/gradle.properties/4.properties", "mod_version");
     private final ModMetadata test5 = new GradlePropertiesModMetadata("https://raw.githubusercontent.com/JamCoreModding/NotifyTesting/main/gradle.properties/5.properties", "different_version_key");
+    private final ModMetadata test6 = new GradlePropertiesModMetadata("https://raw.githubusercontent.com/JamCoreModding/NotifyTesting/main/gradle.properties/6.properties", "mod_version");
 
     /**
      * Check whether everything works under ideal conditions
@@ -22,7 +23,7 @@ public class GradlePropertiesTests {
     @Test
     public void testResolveLatestVersionNormal() throws Exception {
         assertEquals("1.1.0", resolver.resolveLatestVersion(test1, "1.16.5").getFriendlyString()); // Ideal conditions
-        assertEquals("1.1.0", resolver.resolveLatestVersion(test5, "20w41a").getFriendlyString()); // Different version key
+        assertEquals("1.1.0", resolver.resolveLatestVersion(test5, "1.16.5").getFriendlyString()); // Different version key
     }
 
     /**
@@ -42,5 +43,6 @@ public class GradlePropertiesTests {
         assertEquals("1.1.0", resolver.resolveLatestVersion(test2, "1.16.5").getFriendlyString()); // gradle.properties has spaces inbetween key values
         assertEquals("1.1.0", resolver.resolveLatestVersion(test3, "1.16.5").getFriendlyString()); // gradle.properties leading spaces
         assertEquals("1.1.0", resolver.resolveLatestVersion(test4, "1.16.5").getFriendlyString()); // gradle.properties trailing spaces
+        assertEquals("1.1.0", resolver.resolveLatestVersion(test6, "1.16.5").getFriendlyString()); // gradle.properties has all of the above
     }
 }
